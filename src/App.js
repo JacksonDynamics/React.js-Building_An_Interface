70d48bced880e680e310901edd0e5b4787c0c91a
studmanj <bjackso3@hotmail.com>
2022-07-11 17:04:30 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BiArchive } from "react-icons/bi"
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <BiArchive />My Appointments</h1>
+    </div>
+  );
+}
+
+export default App;
