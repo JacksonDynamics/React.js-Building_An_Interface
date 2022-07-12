260d94576a084fa7fda301383ea0a61554ea1f1d
studmanj <bjackso3@hotmail.com>
2022-07-11 20:36:22 -0600

    Add Appointment Info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BiArchive } from "react-icons/bi"
 import Search from "./components/Search"
+import AddAppointment from "./components/addAppointment"
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         <BiArchive />My Appointments!</h1>
+        <AddAppointment />
         <Search />
     </div>
   );
