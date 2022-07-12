4515e0048e9447c088551398d0dd2d91b30e7d7d
studmanj <bjackso3@hotmail.com>
2022-07-11 18:33:35 -0600

    Components Search.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BiArchive } from "react-icons/bi"
+import Search from "./components/Search"
 
 function App() {
   return (
     <div>
       <h1>
         <BiArchive />My Appointments!</h1>
+        <Search />
     </div>
   );
 }
